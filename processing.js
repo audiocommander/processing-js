854120463e53476b7f770d6bf0ecc756f435f07b
Michael Markert <dev@audiocommander.de>
2012-01-26 16:23:43 +0100

    removed default style on size() because it destroys the current style if it's called again after setup(), eg. for dynamic resize

--- processing.js
+++ processing.js
@@ -10149,13 +10149,13 @@
     * @see screen
     */
     DrawingShared.prototype.size = function(aWidth, aHeight, aMode) {
-      if (doStroke) {
-        p.stroke(0);
-      }
-
-      if (doFill) {
-        p.fill(255);
-      }
+//      if (doStroke) {
+//        p.stroke(0);	// This destroys the style if size() is called again after setup()
+//      }
+//
+//      if (doFill) {
+//        p.fill(255);	// This destroys the style if size() is called again after setup()
+//      }
 
       // The default 2d context has already been created in the p.init() stage if
       // a 3d context was not specified. This is so that a 2d context will be
